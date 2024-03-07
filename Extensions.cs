4c6101294540a852999350962bbba730490c4871
Denzyan <striletsden@gmail.com>
2024-03-06 21:25:43 -0600

    HomeWork_13

---
+++ Extensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_2
+{
+    public static class Extensions
+    {
+        public static void WordCounter(this string text)
+        {
+            string[] arrayWords = text.Split(' ');
+
+            Console.WriteLine($"Text has {arrayWords.Length} words");
+        }
+
+        public static bool IsEven(this int number)
+        { 
+            return number % 2 == 0;
+        }
+
+        public static List<string> GetListOfUniqNames(this List<string> names)
+        {
+            return names.Distinct().ToList();
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,25 +1,133 @@
-﻿// HOME WORK "CLASS + METODS"
+﻿using Module_2;
 
-Phone sony = new Phone();
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        //# Исключение и обработка.
+        // Задача 1: Деление на ноль
 
-sony.PhoneBoot();
+        static int Divide()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Enter first number:\t");
+
+                    int num1 = int.Parse(Console.ReadLine());
+
+                    Console.Write("Enter second number:\t");
+
+                    int num2 = int.Parse(Console.ReadLine());
+
+                    return num1 / num2;
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("Not possible to divide by 0");
+
+                    Console.WriteLine("Try again");
+
+                    continue;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Only numbers can be entered");
+
+                    Console.WriteLine("Try again");
+
+                    continue;
+                }
+
+            }
+        }
+
+        Console.WriteLine("\n#TryCatch №1\n");
+
+        Console.WriteLine(Divide());
+
+        // Задача 2: Обработка исключения , открытие файла
+
+        static void FileReader()
+        {
+            try
+            {
+                File.Open("Important.txt", FileMode.Open);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found");
+            }
+
+        }
+
+        // Задача 3: Обработка ошибок при парсинге чисел
+
+        static int Converter()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Enter number:\t");
+
+                    return Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Try again");
+
+                    continue;
+                }
+
+            }
+
+        }
+
+        Console.WriteLine("\n#TryCatch №3\n");
+
+        Console.WriteLine(Converter());
+
+        //Задача 1: Расширение для строк
+
+        string text = "“Granny shifting, not double-clutching like you should”";
+
+        Console.WriteLine("\n#Extension №1\n");
+
+        text.WordCounter();
+
+        // Задача 2: Расширение для чисел
+
+        int number = 241285;
+
+        bool isEven = number.IsEven();
+
+        Console.WriteLine("\n#Extension №2\n");
+
+        Console.WriteLine($"{number} is even: {isEven}");
+
+        // Задача 3: Расширение для списков
+
+        List<string> names = new List<string>()
+        {"John", "Corey", "Michael", "Gideon", "Phoenix", "Donna", "Ramil", "John", "Phoenix"};
+
+        List <string> uniqNames = names.GetListOfUniqNames();
+
+        Console.WriteLine("\n#Extension №3\n");
+
+        foreach (var name in uniqNames)
+        {
+            Console.WriteLine(name);
+        }
+
+    }
 
 
-Phone nokia = new Phone("Nokia", "Android", "Yellow", 333);
+}
 
-nokia.PhoneBoot();
 
-nokia.SetPhonePrice(388);
 
-nokia.SetProvinceTax("qb");
-nokia.SetProvinceTax("bc");
-
-double newprice = nokia.GetPhonePriceWithTax();
-
-Console.WriteLine($"Phone '{nokia.Brand}' price with tax - {newprice}$");
-
-// Так и не разобрался с NumberFormatInfo.CurrentInfo.CurrencySymbol
-//  ? Возможно причина в локации.
 
 
 
